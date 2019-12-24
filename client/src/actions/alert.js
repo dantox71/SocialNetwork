7b92a22021327fb4f1b654be770b0ff8c21e50c2
Daniel <Daniellagowskii@gmail.com>
2019-12-24 12:17:30 +0100

    Add reducer and actions for alert

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,2 +1,19 @@
 import axios from "axios";
 import { SET_ALERT, REMOVE_ALERT } from "./types";
+import uuid from "uuid";
+
+export const setAlert = (msg, type, timeout = 3000) => dispatch => {
+  const id = uuid.v4();
+
+  dispatch({
+    SET_ALERT,
+    payload: { msg, type, id }
+  });
+
+  setTimeout(() => {
+    dispatch({
+      type: REMOVE_ALERT,
+      payload: id
+    });
+  }, timeout);
+};
