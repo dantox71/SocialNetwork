32a759fd8851aabde2bb791bf556009bdf6b2341
Daniel <Daniellagowskii@gmail.com>
2019-12-24 10:31:54 +0100

    Add logout functionality and

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,9 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import { Link } from "react-router-dom";
-import { set } from "mongoose";
+import { connect } from "react-redux";
+import { logout } from "../../actions/auth";
 
-const Navbar = props => {
+const Navbar = ({ isAuthenticated, logout }) => {
   const [menuState, setMenuState] = useState(false);
+
+  const guestLinks = (
+    <Fragment>
+      <li className="nav-item">
+        <Link to="/" className="nav-link btn ml-2">
+          Home
+        </Link>
+      </li>
+
+      <li className="nav-item">
+        <Link to="/posts" className="nav-link btn ml-2">
+          Posts
+        </Link>
+      </li>
+    </Fragment>
+  );
+
+  const authLinks = (
+    <Fragment>
+      <li className="nav-item ">
+        <Link to="/profile" className="nav-link btn  ">
+          <img
+            src="https://avatars1.githubusercontent.com/u/53446414?s=460&v=4"
+            alt="User avatar"
+            className="mr-1"
+          />
+          My profile
+        </Link>
+      </li>
+
+      <li className="nav-item">
+        <Link to="/profiles" className="nav-link btn  ">
+          Profiles
+        </Link>
+      </li>
+
+      <li className="nav-item">
+        <Link to="/posts" className="nav-link btn  ">
+          Posts
+        </Link>
+      </li>
+
+      <li className="nav-item">
+        <a href="#!" onClick={() => logout()} className="nav-link btn">
+          Logout <i className="ml-1 fa fa-sign-out-alt"></i>
+        </a>
+      </li>
+    </Fragment>
+  );
 
   return (
     <nav className="main-nav">
@@ -15,19 +65,7 @@ const Navbar = props => {
         </Link>
       </div>
 
-      <ul className="nav-list">
-        <li className="nav-item">
-          <Link to="/" className="nav-link btn">
-            Home
-          </Link>
-        </li>
-
-        <li className="nav-item">
-          <Link to="/posts" className="nav-link btn">
-            Posts
-          </Link>
-        </li>
-      </ul>
+      <ul className="nav-list">{isAuthenticated ? authLinks : guestLinks}</ul>
       <div
         className={menuState ? "opened mobile-toggler" : "mobile-toggler"}
         onClick={() => setMenuState(!menuState)}
@@ -36,20 +74,14 @@ const Navbar = props => {
       </div>
 
       <ul className={menuState ? "opened mobile-nav-list" : "mobile-nav-list"}>
-        <li className="nav-item">
-          <Link to="/" className="nav-link btn ml-2">
-            Home
-          </Link>
-        </li>
-
-        <li className="nav-item">
-          <Link to="/posts" className="nav-link btn ml-2">
-            Posts
-          </Link>
-        </li>
+        {isAuthenticated ? authLinks : guestLinks}
       </ul>
     </nav>
   );
 };
 
-export default Navbar;
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { logout })(Navbar);
