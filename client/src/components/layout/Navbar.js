4b71a2a8b6db158f3110b2d243fe30f8651adce9
Daniel <Daniellagowskii@gmail.com>
2019-12-28 19:38:09 +0100

    Update navbar links.Now if link is clicked - navbar will close .

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -2,43 +2,43 @@ import React, { useState, Fragment, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { logout } from "../../actions/auth";
-import { getProfile } from "../../actions/profiles";
+import { getCurrentProfile } from "../../actions/profiles";
 
 import PropTypes from "prop-types";
 
 const Navbar = ({
   auth: { isAuthenticated, user },
   logout,
-  getProfile,
-  profile
+  getCurrentProfile,
+  currentProfile
 }) => {
   const [menuState, setMenuState] = useState(false);
 
   useEffect(() => {
     if (user !== null) {
-      getProfile();
+      getCurrentProfile();
     }
   }, [user]);
 
   const guestLinks = (
     <Fragment>
-      <li className="nav-item">
+      <li className="nav-item" onClick={() => setMenuState(false)}>
         <Link to="/" className="nav-link btn ml-2">
           Home
         </Link>
       </li>
-      <li className="nav-item">
+      <li className="nav-item" onClick={() => setMenuState(false)}>
         <Link to="/register" className="nav-link btn ml-2">
           Sign Up
         </Link>
       </li>
-      <li className="nav-item">
+      <li className="nav-item" onClick={() => setMenuState(false)}>
         <Link to="/login" className="nav-link btn ml-2">
           Sign In
         </Link>
       </li>
 
-      <li className="nav-item">
+      <li className="nav-item" onClick={() => setMenuState(false)}>
         <Link to="/posts" className="nav-link btn ml-2">
           Posts
         </Link>
@@ -48,31 +48,46 @@ const Navbar = ({
 
   const authLinks = (
     <Fragment>
-      <li className="nav-item ">
-        {profile ? (
-          <Link to={`/profile/${profile._id}`} className="nav-link btn  ">
-            <img src={profile.avatar} alt="User avatar" className="mr-1" />
+      <li className="nav-item " onClick={() => setMenuState(false)}>
+        {currentProfile ? (
+          <Link
+            to={`/profile/${currentProfile._id}`}
+            className="nav-link btn  "
+          >
+            <img
+              src={currentProfile.avatar}
+              alt="User avatar"
+              className="mr-1"
+            />
             My profile
           </Link>
         ) : (
-          <Link to="/create-profile">Create Profile</Link>
+          <Link to="/create-profile" class="nav-link btn  ">
+            Create Profile
+          </Link>
         )}
       </li>
 
-      <li className="nav-item">
+      <li className="nav-item" onClick={() => setMenuState(false)}>
         <Link to="/profiles" className="nav-link btn  ">
           Profiles
         </Link>
       </li>
 
-      <li className="nav-item">
+      <li className="nav-item" onClick={() => setMenuState(false)}>
         <Link to="/posts" className="nav-link btn  ">
           Posts
         </Link>
       </li>
 
-      <li className="nav-item">
-        <a href="#!" onClick={() => logout()} className="nav-link btn">
+      <li className="nav-item" onClick={() => setMenuState(false)}>
+        <a
+          href="#!"
+          onClick={() => {
+            logout();
+          }}
+          className="nav-link btn"
+        >
           Logout <i className="ml-1 fa fa-sign-out-alt"></i>
         </a>
       </li>
@@ -107,13 +122,13 @@ const Navbar = ({
 const propTypes = {
   logout: PropTypes.func.isRequired,
   isAuthenticated: PropTypes.bool.isRequired,
-  getProfile: PropTypes.func.isRequired,
-  profile: PropTypes.object.isRequired
+  getCurrentProfile: PropTypes.func.isRequired,
+  currentProfile: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
   auth: state.auth,
-  profile: state.profiles.profile
+  currentProfile: state.profiles.currentProfile
 });
 
-export default connect(mapStateToProps, { logout, getProfile })(Navbar);
+export default connect(mapStateToProps, { logout, getCurrentProfile })(Navbar);
